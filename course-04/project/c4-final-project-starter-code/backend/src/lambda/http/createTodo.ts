92b2ea2b1596c1da5588a493612efe63c143ef22
= <mwatermanaus@users.noreply.github.com>
2021-08-08 17:35:46 +0100

    Workin get Todo list and create ToDo

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/createTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/createTodo.ts
@@ -1,34 +1,47 @@
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
-// import * as AWS  from 'aws-sdk'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import * as AWS  from 'aws-sdk'
+import * as uuid from 'uuid'
+import * as middy from 'middy'
+import { cors, httpErrorHandler } from 'middy/middlewares'
 
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
 import { createLogger } from '../../utils/logger'
-// import { TodoItem } from '../../models/TodoItem'
+import { TodoItem } from '../../models/TodoItem'
+import { getUserId } from '../utils'
 
 const logger = createLogger('createTodos')
-// const docClient = new AWS.DynamoDB.DocumentClient()
+const docClient = new AWS.DynamoDB.DocumentClient()
 
-// const todosTable = process.env.TODOS_TABLE
-// const todoBucket = process.env.ATTACHMENTS_S3_BUCKET
+const todosTable = process.env.TODOS_TABLE
+//const todoBucket = process.env.ATTACHMENTS_S3_BUCKET
 
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-
+export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
 
   // TODO: Implement creating a new TODO item
- // const timestamp = new Date().toISOString()
+  const timestamp = new Date().toISOString()
   const newTodo: CreateTodoRequest = JSON.parse(event.body)
+  const todoId = uuid.v4()
 
-  logger.info('Creating new item ', newTodo )
+  logger.info('Creating new item ' + newTodo.name)
 
-//  const todoItem:TodoItem = 
+  const userId = getUserId(event)
+  logger.info('Creating new item - userId ' + userId)
 
-  // await docClient.put({
-  //   TableName: todosTable,
-  //   Item: todoItem
-  // }).promise()
+  const todoItem:TodoItem = {
+    userId: userId,
+    todoId: todoId,
+    createdAt: timestamp,
+    ...newTodo,
+    done: false
+  }
+
+  await docClient.put({
+    TableName: todosTable,
+    Item: todoItem
+  }).promise()
 
 
   return {
@@ -37,6 +50,15 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Credentials': true
     },
-    body: JSON.stringify({})
+    body: JSON.stringify(todoItem)
   }
-}
+})
+
+
+handler
+ .use(httpErrorHandler())
+ .use(
+     cors({
+       credentials: true
+  })
+)
--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
@@ -1,23 +1,63 @@
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import { createLogger } from '../../utils/logger'
+import * as AWS  from 'aws-sdk'
+import * as middy from 'middy'
+
+import { cors, httpErrorHandler } from 'middy/middlewares'
+import { getUserId } from '../utils'
 
 const logger = createLogger('getTodos')
-// const docClient = new AWS.DynamoDB.DocumentClient()
+const docClient = new AWS.DynamoDB.DocumentClient()
 
-// const todosTable = process.env.TODOS_TABLE
+const todosTable = process.env.TODOS_TABLE
+const indexTable = process.env.TODOS_CREATED_AT_INDEX
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   // TODO: Get all TODO items for a current user
   logger.info('Processing todo event', event)
-  
+  const userId = getUserId(event)
+
+  const todoList = await getTaskForUser(userId)
+
+  logger.info('Returned this list from DB todo event' + JSON.stringify(todoList))
+
   return  {
     statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Credentials': true
     },
-    body: JSON.stringify({})
+    body: JSON.stringify({
+      "items": todoList
+    })
   }
 }
+)
+
+
+async function getTaskForUser(userId: string) {
+  const result = await docClient.query({
+    TableName: todosTable,
+    IndexName: indexTable,
+    KeyConditionExpression: 'userId = :userId',
+    ExpressionAttributeValues: {
+      ':userId': userId
+    },
+    ScanIndexForward: false
+  }).promise()
+
+  return result.Items
+}
+
+
+
+
+handler
+ .use(httpErrorHandler())
+ .use(
+     cors({
+       credentials: true
+  })
+)
